e1edb0d4149eabc0590b87ad8fbbcd5912e74033
Dave <dave@home>
2022-03-02 18:21:30 -0800

    cleaned up

--- setup.bash
+++ setup.bash
@@ -3,10 +3,14 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y podman runit runit-systemd
 
-mkdir -p /etc/service/podman-postgres
+export RUN=/etc/service/podman-postgres/run
 
-cat > /etc/service/podman-postgres/run << thatsall
+mkdir -p ${RUN%/*}
+
+sudo cat > $RUN << thatsall
 #! /bin/bash
 exec podman run --rm -e POSTGRES_PASSWORD=mysecretpassword --network host docker.io/library/postgres postgres
 thatsall
+
+sudo chmod +x $RUN
 
